22b12b633432c651a2eed1713fd28f18d1fff964
Lalamee <46878961+Lalamee@users.noreply.github.com>
2023-01-10 02:36:47 +0300

    Update Bird.cs

--- MyFlappyBird/Assets/Scripts/Bird/Bird.cs
+++ MyFlappyBird/Assets/Scripts/Bird/Bird.cs
@@ -5,7 +5,7 @@ using UnityEngine.Events;
 public class Bird : MonoBehaviour
 {
     private BirdMover _mover;
-    private int _score = 0;
+    private int _score;
 
     public event UnityAction GameOver;
     public event UnityAction<int> ScoreChanged; 
